181cc223b8f2647a51251529e8673fd67be6f388
ONLYemme <65762951+ONLYemme@users.noreply.github.com>
2021-12-24 19:06:28 +0100

    client/server

--- src/controllofile/ControlloFile.java
+++ src/controllofile/ControlloFile.java
@@ -2,12 +2,18 @@ package controllofile;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Calendar;
+import java.util.Date;
 
 
 public class ControlloFile {
     
-    public static void log( String home, String username, String hostname, String ip, long data ) {
+    public static void log( String home, String username, String hostname, String ip, String data ) {
         
         File file = new File( home + "\\cb.log");
         
@@ -30,9 +36,23 @@ public class ControlloFile {
         
     }
     
-    public static void invia( String home, String username, String hostname, String ip, long data, String pathImg ) {
-        
-        // comandi per inviare
+    public static void invia( String home, String username, String hostname, String ip, String data, String pathImg ) {
+        String hostName = "192.168.1.220";
+        int portNumber = 4444;
+        try (
+                Socket kkSocket = new Socket(hostName, portNumber);
+                PrintWriter out = new PrintWriter(kkSocket.getOutputStream(), true);
+        ) {
+            out.println(home + " "+ username+ " " +hostname+ " " + ip+ " " + data);
+
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " +
+                    hostName);
+            System.exit(1);
+        }
         
     }
 
@@ -42,9 +62,9 @@ public class ControlloFile {
         String ip;
         String username;
         String home;
-        
 
-        long data;
+        Date dataObject = new Date();
+        String data;
         
         //dati rete
         try {
@@ -62,17 +82,23 @@ public class ControlloFile {
 
 
         String path1 = home + "\\Desktop\\prova.txt";
+        path1 = "/Users/mattia/Desktop";
 
         File file = new File( path1);
         long mod = file.lastModified();
-        
+
+
         while (true) {
             if ( mod!=file.lastModified() ) {
                 mod = file.lastModified();
-                data = System.currentTimeMillis();
+                //data = System.currentTimeMillis();
+                data = dataObject.getDate() + "/" + (dataObject.getMonth()+1) + "/" + (dataObject.getYear()+1900) + " " +dataObject.getHours() + ":"+ dataObject.getMinutes();
                 log( home, username, hostname, ip, data);
                 invia( home, username, hostname, ip, data, "ciao");
                 System.out.println("modificato");
+
+            } else {
+
             }
             Thread.sleep(2*1*1000); // aspetta 2 minuti
         }
