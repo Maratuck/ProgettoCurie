7910c92a6a29a93d4c8e278ef10954532c3bd7be
enne139 <54336269+enne139@users.noreply.github.com>
2021-12-25 20:53:00 +0100

    agginta file daInviare

--- src/controllofile/ControlloFile.java
+++ src/controllofile/ControlloFile.java
@@ -1,42 +1,58 @@
 package controllofile;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.Calendar;
 import java.util.Date;
 
 
 public class ControlloFile {
     
-    public static void log( String home, String username, String hostname, String ip, String data ) {
+    static String PTcartella = "\\BAA"; 
+    static String hostname;
+    static String ip;
+    static String username;
+    static String home;
+    static int cNI;                  // contatore messagi non inviati
+    static boolean ckRI;             // variabile per vederese se si sta provando a inviare
+    static boolean ckI;              // variabile per vederese se si puo iniare
+    
+    public static void log( String data ){
         
-        File file = new File( home + "\\cb.log");
-        
+        File file = new File( home + PTcartella + "\\cb.log");
         
         try {
-            if ( file.exists() ) {
-                file.createNewFile();
-            }
-        } catch ( Exception e) {
-            System.err.println("errore creazione file");
-        }
-            
-        try {
-            FileWriter fr = new FileWriter( file, true);
-            fr.append( "\"" + home + "\"" + ";" + "\""+ username + "\"" + ";" + "\"" + hostname + "\"" + ";" + "\"" + ip + "\"" + ";" + data + "\n");
-            fr.close();
+            BufferedWriter br = new BufferedWriter( new FileWriter( file, true) );
+            br.append( "\"" + home + "\"" + ";" + "\""+ username + "\"" + ";" + "\"" + hostname + "\"" + ";" + "\"" + ip + "\"" + ";" + "\"" + data + "\"" +"\n");
+            br.close();
         } catch (Exception e) {
-            System.err.println("errore aggiunta file");
+            System.err.println("errore aggiunta file log");
+        } finally {
         }
-        
     }
     
-    public static void invia( String home, String username, String hostname, String ip, String data) {
+    public static void salvMan( String data ){
+        
+        File file = new File( home + PTcartella + "\\ma.log");
+        
+        try {
+            BufferedWriter br = new BufferedWriter( new FileWriter( file, true) );
+            br.append( "\"" + home + "\"" + ";" + "\""+ username + "\"" + ";" + "\"" + hostname + "\"" + ";" + "\"" + ip + "\"" + ";" + "\"" + data + "\"" +"\n");
+            br.close();
+        } catch (Exception e) {
+            System.err.println("errore aggiunta file manc");
+        } finally {
+        }
+    }
+    
+    public static boolean invia( String data) {
         String hostName = "192.168.1.220";
         int portNumber = 4444;
         try (
@@ -44,27 +60,22 @@ public class ControlloFile {
                 PrintWriter out = new PrintWriter(kkSocket.getOutputStream(), true);
         ) {
             out.println(home + " "+ username+ " " +hostname+ " " + ip+ " " + data);
-
+            return true;
+            
         } catch (UnknownHostException e) {
             System.err.println("Don't know about host " + hostName);
-            System.exit(1);
         } catch (IOException e) {
-            System.err.println("Couldn't get I/O for the connection to " +
-                    hostName);
-            System.exit(1);
+            System.err.println("Couldn't get I/O for the connection to " + hostName);
         }
-        
+        return false;
     }
 
     public static void main(String[] args) throws InterruptedException {
         
-        String hostname;
-        String ip;
-        String username;
-        String home;
-
-        Date dataObject = new Date();
         String data;
+        cNI = 0;
+        ckI = true;
+        ckRI = false;
         
         //dati rete
         try {
@@ -79,9 +90,53 @@ public class ControlloFile {
         // dati pc 
         username = System.getProperty("user.name");
         home = System.getProperty("user.home");
-
-
-        String path1 = home + "\\Desktop\\prova.txt";
+        
+        // cartella controllo
+        try {
+            File cartella = new File( home + PTcartella);
+            if ( !cartella.exists() || !cartella.isFile() ) {
+                cartella.mkdir();
+            }
+        } catch ( Exception e) {
+            System.err.println("errore creazione cartella");
+        } 
+        
+        // file log controllo
+        try {
+            File file = new File( home + PTcartella + "\\cb.log");
+            if ( !file.exists() ) {
+                file.createNewFile();
+            }
+        } catch ( Exception e) {
+            System.err.println("errore creazione file log");
+        }
+        
+        // file manc controllo
+        try {
+            File file = new File( home + PTcartella + "\\ma.log");
+            if ( !file.exists() ) {
+                file.createNewFile();
+            }
+        } catch ( Exception e) {
+            System.err.println("errore creazione file mac");
+        }
+        
+        // controlla messagi mancanti
+        try {
+            BufferedReader bw = new BufferedReader( new FileReader( home + PTcartella + "\\ma.log"));
+            System.out.println("prova");
+            String linea;
+            while ( (linea = bw.readLine()) != null) {
+                cNI ++;
+            }
+            bw.close();
+        } catch ( Exception e) {
+            System.err.println("errore lettura mancanti");
+        }
+        
+            
+        //String path1 = home + "\\Desktop\\prova.txt";
+        String path1 = home + "\\AppData\\Roaming\\Microsoft\\Windows\\Themes\\TranscodedWallpaper";
 
         File file = new File( path1);
         long mod = file.lastModified();
@@ -91,13 +146,15 @@ public class ControlloFile {
             if ( mod!=file.lastModified() ) {
                 mod = file.lastModified();
                 //data = System.currentTimeMillis();
+                Date dataObject = new Date();
                 data = dataObject.getDate() + "/" + (dataObject.getMonth()+1) + "/" + (dataObject.getYear()+1900) + " " +dataObject.getHours() + ":"+ dataObject.getMinutes();
-                log( home, username, hostname, ip, data);
-                invia( home, username, hostname, ip, data);
+                log( data);
+                if ( !invia( data) ) {
+                    System.out.println("non inviato");
+                    salvMan( data);       
+                    cNI ++;
+                }
                 System.out.println("modificato");
-
-            } else {
-
             }
             Thread.sleep(2*1*1000); // aspetta 2 minuti
         }
