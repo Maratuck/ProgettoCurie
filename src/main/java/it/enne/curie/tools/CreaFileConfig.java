f3c6e93698a188faa38e5b6a56a74a8089acb47f
enne139 <enne888139@gmail.com>
2022-03-26 19:01:31 +0100

    implementazione CreaFileConfig

--- src/main/java/it/enne/curie/tools/CreaFileConfig.java
+++ src/main/java/it/enne/curie/tools/CreaFileConfig.java
@@ -9,18 +9,44 @@ import java.io.IOException;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 
 public class CreaFileConfig {
 
     public static void main(String[] args) {
 
+        String ip;
+        int port;
+        int x, y;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("inserire ip server :");
+        ip = scan.nextLine();
+        System.out.print("inserire porta :");
+        port = scan.nextInt();
+        System.out.print("inserire numero di banchi per riga (X) :");
+        x = scan.nextInt();
+        System.out.print("inserire numero di banchi per colonna (Y) :");
+        y = scan.nextInt();
+        System.out.println("---------------------------------------");
+        System.out.println("ip : " + ip);
+        System.out.println("port : " + port);
+        System.out.println("X : " + x);
+        System.out.println("Y : " + y);
+        System.out.println("---------------------------------------");
+
+        File file = new File("./config.mkt");
 
         try {
-            System.out.println( CustomExtension.readDecoded(new File(CuriePaths.getConfigPath()))[1] );
+            file.delete();
+            file.createNewFile();
+            CustomExtension.writeEncoded( new String[]{ip, Integer.toString(port), Integer.toString(x), Integer.toString(y)}, file);
         } catch (IOException e) {
-            e.printStackTrace();
+            System.err.println("errore creazione file");
         }
+        System.out.println("file creato e scritto");
 
     }
 
